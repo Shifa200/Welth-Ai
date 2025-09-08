e7bb6542f6c055a9ab11db8c916fcabce960c047
Shifa200 <shifa20naqvi@gmail.com>
2025-09-08 13:41:13 +0530

    added arcjet

--- my-app/actions/send-email.js
+++ my-app/actions/send-email.js
@@ -1,14 +1,17 @@
 "use server";
 
+import { render } from "@react-email/render";
 import { Resend } from "resend";
 
 export async function sendEmail({ to, subject, react }) {
   const resend = new Resend(process.env.RESEND_API_KEY || "");
 
   try {
+
+    const html = render(react);
     const data = await resend.emails.send({
       from: "Finance App <onboarding@resend.dev>",
-      to,
+      to: "shifa20naqvi@gmail.com",
       subject,
       react,
     });
